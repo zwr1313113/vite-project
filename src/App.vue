ec64a3764efd1587a7f9e3bd68148e150198fa43
zhangwr <zhangwr.fnst@fujitsu.com>
2022-07-26 14:12:19 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<script lang="ts">
+import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  components: { HelloWorld },
+  setup() {
+    let isDone: boolean = false;
+    let decLiteral: number = 6;
+    let name: string = "bob";
+    let list: number[] = [1, 2, 3];
+    let list1: Array<number> = [1, 2, 3];
+    let x: [string, number];
+    x = ["hello", 10];
+    let notSure: any = 4;
+  },
+});
+</script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
